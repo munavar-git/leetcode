8cef636fc7b98d00c8132e2c892546551f7869b5
Munavar <mmunavar66@gmail.com>
2026-07-18 13:59:01 +0530

    Implement function to find second largest digit

---
+++ 1796-second-largest-digit-in-a-string/1796-(solution2)second-largest-digit-in-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var secondHighest = function(s) {
+  let num=s.match(/\d/g)
+  let uniquenum=[...new Set(num)]
+  let sorted=uniquenum.sort((a,b)=>b-a)
+  if(sorted.length< 2){
+    return -1
+  }{
+    return Number(sorted[1])
+  } 
+}
