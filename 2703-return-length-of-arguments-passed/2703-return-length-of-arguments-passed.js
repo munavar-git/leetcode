99bee265c8bec6411f239480c1d8a471399f0850
Munavar <mmunavar66@gmail.com>
2026-07-29 09:56:09 +0530

    Time: 47 ms (32.69%), Space: 53.6 MB (55.18%) - LeetHub

--- 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
+++ 2703-return-length-of-arguments-passed/2703-return-length-of-arguments-passed.js
@@ -3,7 +3,11 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
-  return args.length
+    let count=0
+    for(let i=0;i<args.length;i++){
+        count++
+    }
+    return count
 };
 
 /**
