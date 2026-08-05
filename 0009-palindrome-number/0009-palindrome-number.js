f4661d0f3f52d498f46038669cc857c13b9ba459
Munavar <mmunavar66@gmail.com>
2026-08-05 13:35:11 +0530

    Time: 11 ms (24.81%), Space: 65 MB (27.24%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.js
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -3,6 +3,6 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let reversed= x.toString().split("").reverse().join("");
+    let reversed=x.toString().split("").reverse().join("");
     return x==reversed
 };
