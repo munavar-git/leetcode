211f422b52f24b5a0d7098019ed4640dfa6ebae4
Munavar <mmunavar66@gmail.com>
2026-09-18 10:48:05 +0530

    Time: 40 ms (93.39%), Space: 57.4 MB (28.57%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,28 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let number=init
+
+    function increment(){
+        return ++number;
+    }
+
+    function decrement(){
+        return --number
+    }
+
+    function reset(){
+        return (number=init)
+    }
+    
+    return {increment,decrement,reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
